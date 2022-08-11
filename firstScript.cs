7300fca8241c6a96ee8d7332774b53b8528f9011
Charles <charles.goedefroit@gmail.com>
2022-08-11 19:42:46 +0200

    Update firstScript.cs

--- firstScript.cs
+++ firstScript.cs
@@ -4,4 +4,11 @@ public Program() {
 
 public void Main(string argument, UpdateType updateSource) {
     Echo("Hello, world!"); // Print "Hello, world!" in the console
+    
+    // Echo : Action<string> => Echo(string); Prints out text onto the currently running programmable block's detail info area.
+    // Me : IMyProgrammableBlock; reference of the current running programmable block.
+    // Runtime : IMyGridProgramRuntimeInfo; Gets runtime information for the running grid program.
+    // Storage : string; Storage sting to store data between game sessions.
+    // IGC : IMyIntergridCommunicationSystem; IGC instance
+    // GridTerminalSystem : IMyGridTerminalSystem; Provides access to the grid terminal system as viewed from this programmable block.
 }
